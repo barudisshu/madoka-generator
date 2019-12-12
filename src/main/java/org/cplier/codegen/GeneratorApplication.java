9fdb393c957447d21ffc351a6e3f0232260a43b6
Galudisu <galudisu@gmail.com>
2019-12-12 15:41:09 +0800

    添加页面

--- src/main/java/org/cplier/codegen/GeneratorApplication.java
+++ src/main/java/org/cplier/codegen/GeneratorApplication.java
@@ -1,7 +1,12 @@
 package org.cplier.codegen;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GeneratorApplication {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(GeneratorApplication.class, args);
+    }
 }
